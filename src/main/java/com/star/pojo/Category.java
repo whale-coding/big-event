58b4faf4d68cf1c611c2253f0127c3a140e63f56
whale-coding <njq3699@gmail.com>
2024-12-02 16:51:30 +0800

    新增文章分类、文章分类列表、文章分类详情、更新文章分类接口

--- src/main/java/com/star/pojo/Category.java
+++ src/main/java/com/star/pojo/Category.java
@@ -1,14 +1,22 @@
 package com.star.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 @Data
 public class Category {
+    @NotNull
     private Integer id; // 主键ID
+    @NotEmpty  // validation非空校验
     private String categoryName; // 分类名称
+    @NotEmpty  // validation非空校验
     private String categoryAlias; // 分类别名
     private Integer createUser; // 创建人ID
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")  // 日期格式化
     private LocalDateTime createTime; // 创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")  // 日期格式化
     private LocalDateTime updateTime; // 更新时间
 }
